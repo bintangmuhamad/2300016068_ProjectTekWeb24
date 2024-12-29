a2fdc4b92dc72402f7e89c6c8c0e6968a9af86b7
bintangmuhamad <mbintangjaya2886@gmail.com>
2024-12-29 21:19:49 +0700

    Proggres 4

--- src/components/Report.js
+++ src/components/Report.js
@@ -10,7 +10,7 @@ function Report() {
     <div className="p-6">
       <h2 className="text-2xl font-bold text-greenGoPalm mb-4">Laporan Perawatan & Panen</h2>
       <p>Ini adalah halaman laporan yang akan menampilkan detail perawatan dan panen dalam format yang lebih ringkas.</p>
-      {/* Anda bisa menambahkan grafik, tabel, atau visualisasi data lainnya di sini */}
+      {/* Target dapat menambahkan grafik, tabel, atau visualisasi data lainnya di sini */}
     </div>
   );
 }
